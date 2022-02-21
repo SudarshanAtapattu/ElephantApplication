b4c675c0715971859e2f73b9ff0a75a760065783
SudarshanaAtapattu <45549729+PradeepaSudarshanaAtapattu@users.noreply.github.com>
2022-02-22 01:48:32 +0530

    Add New Menu

---
+++ app/src/main/java/com/newapp/elephantapplication/Menue.java
@@ -0,0 +1,14 @@
+package com.newapp.elephantapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menue);
+    }
+}
