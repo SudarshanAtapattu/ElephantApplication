3cf502183d5731714202cb7805ceabd41a755c8e
SudarshanaAtapattu <45549729+PradeepaSudarshanaAtapattu@users.noreply.github.com>
2022-02-22 02:06:36 +0530

    Live location LiveLocationActivity Add

---
+++ app/src/main/java/com/newapp/elephantapplication/LiveLocationActivity.java
@@ -0,0 +1,14 @@
+package com.newapp.elephantapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LiveLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_location_acivity);
+    }
+}
